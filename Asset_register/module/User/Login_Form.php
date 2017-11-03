0f8d7cca6454b48db9dfad8bf1c4cc7dc4ab3510
Sittichai  Wongfun <sittichai_wo57@live.rmutl.ac.th>
2017-11-03 16:02:51 +0700

    แกมาแล้ว ใหม่มาก

---
+++ Asset_register/module/User/Login_Form.php
@@ -0,0 +1,22 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Login</title>
+<link href="../../CSS/Login.css" rel="stylesheet" type="text/css">
+<link href="../../layout.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+<form method="post" action="module\user\Check_Login.php" >
+<!--<P><center><img src="img/if_user_accounts.png" style="width:118px;height:118px;" >
+</center></P>-->
+<h4><P>Username: <input type="text" name="username" id="uses" required="required" ></P>
+<P>Password: <input type="password" name="passwd" id="pass" required="required" ></P>
+<center><button type="submit" name="" value="Login" style="width:auto;">Login</button>
+<button type="reset" name="" value="Cancel" class="b2" style="width:auto;">Cancel</button>
+</h4></center>
+</form>
+</body>
+</html>
